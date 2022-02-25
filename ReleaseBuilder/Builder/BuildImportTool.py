ad42f9fdfcaa5600d2aa1e834a8636b41b2ea859
Wesley Buchanan <wesley.buchanan@br-automation.com>
2022-02-25 16:22:54 -0500

    alternative path for msbuild tool

--- ReleaseBuilder/Builder/BuildImportTool.py
+++ ReleaseBuilder/Builder/BuildImportTool.py
@@ -12,6 +12,9 @@ buildOutput = rf'FrameworkImporter\bin\Release'
 def BuildImportTool(dir, outputDir) -> bool:
     global msbuild, solution, buildOutput
     
+    if (os.path.exists(msbuild) == False)
+        msbuild = r'C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe'
+
     args = '-p:Configuration=Release;optimize=true;DebugSymbols=false;DebugType=None;PreBuildEvent='
     result = subprocess.run(shlex.split(f'"{msbuild}" "{dir}\\{solution}" "{args}"'))
 
