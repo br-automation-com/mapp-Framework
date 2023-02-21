e0f8bfd07052140d4a25969630679e606d47fffd
austin_justin <justin.austin@br-automation.com>
2023-02-21 09:14:35 -0500

    Basic Audit unit test

--- mappFramework/Logical/UnitTest/Audit/AuditTest/Set_AuditMgr.c
+++ mappFramework/Logical/UnitTest/Audit/AuditTest/Set_AuditMgr.c
@@ -1,4 +1,5 @@
 #include <bur/plctypes.h>
+#include <fileio.h>
 
 #ifdef _DEFAULT_INCLUDES
 #include <AsDefault.h>
@@ -7,6 +8,20 @@
 #include "UnitTest.h"
 #include <stdbool.h>
 #include <string.h>
+
+#define TIMEOUT_TEST_CASE									\
+	if (cycleCount >= 254)									\
+	{														\
+		char abortMessage[80];								\
+		char substate[10];									\
+		memset(abortMessage, 0, sizeof(abortMessage));		\
+		memset(substate, 0, sizeof(substate));				\
+		itoa(ActSubState, substate, 10);					\
+		strcpy(abortMessage, "Timeout in ActSubState = ");	\
+		strcat(abortMessage, substate);						\
+		TEST_FAIL(abortMessage);							\
+		TEST_DONE;											\
+	}
 
 _SETUP_SET(void)
 {
@@ -21,6 +36,12 @@ _TEARDOWN_SET(void)
 
 _SETUP_TEST(void)
 {
+	ArrangeSubState = 0;
+	ActSubState = 0;
+	AssertSubState = 0;
+	SampleTemperature = 1;
+	NumberOfFiles = 0;
+	UnitTestState = TEST_ARRANGE;
     TEST_DONE;
 }
 
@@ -29,16 +50,72 @@ _TEARDOWN_TEST(void)
 	TEST_DONE;
 }
 
+_CYCLIC_SET(void)
+{
+	cycleCount++;
+	DirInfo(&DirInfo_0);
+}
+
+_TEST ExportArchive(void)
+{
+	TIMEOUT_TEST_CASE;
+//	UnitTestState = TEST_ASSERT;
+	switch (UnitTestState)
+	{
+		case TEST_ARRANGE:
+			SampleTemperature++;
+			DirInfo_0.enable = 1;
+			DirInfo_0.pDevice = (UDINT) "mappAuditFiles";
+			DirInfo_0.pPath = 0;
+			DirInfo(&DirInfo_0);
+			TEST_BUSY_CONDITION(!HMIAuditInterfaceCtrl.Status.AuditTrailArchiveAvailable);
+			NumberOfFiles = DirInfo_0.filenum;
+			UnitTestState = TEST_ACT;
+			break;
+		
+		case TEST_ACT:
+			// Archive audit
+			HMIAuditInterfaceCtrl.Commands.ExportArchives = 1;
+			TEST_BUSY_CONDITION(!HMIAuditInterfaceCtrl.Status.AuditTrailCmdDone);
+			HMIAuditInterfaceCtrl.Commands.ExportArchives = 0;
+			DirInfo(&DirInfo_0);
+			UnitTestState = TEST_ASSERT;
+			break;
+		
+		case TEST_ASSERT:
+			// Check save location for archive
+			TEST_ASSERT(NumberOfFiles + 1 == DirInfo_0.filenum);
+			TEST_DONE;
+			break;
+	}
+	DirInfo(&DirInfo_0);
+}
+
+_TEST AutomaticArchive(void)
+{
+	TIMEOUT_TEST_CASE;
+	
+	// Arrange
+	
+	// Act
+	
+	// Assert
+	
+	TEST_DONE;
+}
+
 /*
 B+R UnitTest: This is generated code.
 Do not edit! Do not move!
 Description: UnitTest Testprogramm infrastructure (TestSet).
-LastUpdated: 2022-09-26 13:08:18Z
+LastUpdated: 2023-02-06 16:23:26Z
 By B+R UnitTest Helper Version: 2.0.1.59
 */
 UNITTEST_FIXTURES(fixtures)
 {
+	new_TestFixture("ExportArchive", ExportArchive), 
+	new_TestFixture("AutomaticArchive", AutomaticArchive), 
 };
 
-UNITTEST_CALLER_COMPLETE_EXPLICIT(Set_AuditMgr, "Set_AuditMgr", setupTest, teardownTest, fixtures, setupSet, teardownSet, 0);
+UNITTEST_CALLER_COMPLETE_EXPLICIT(Set_AuditMgr, "Set_AuditMgr", setupTest, teardownTest, fixtures, setupSet, teardownSet, cyclicSetCaller);
 
---
+++ mappFramework/Logical/UnitTest/Audit/AuditTest/Types.typ
@@ -0,0 +1,72 @@
+
+TYPE
+	TestStateEnum : 
+		(
+		TEST_ARRANGE := 10,
+		TEST_ACT := 20,
+		TEST_ASSERT := 30
+		);
+END_TYPE
+
+(*UserX control*)
+
+TYPE
+	UserXLogin_type : 	STRUCT 
+		Commands : UserXLoginCommands_type;
+		Status : UserXLoginStatus_type;
+		Parameters : UserXLoginParameters_type;
+	END_STRUCT;
+	UserXLoginCommands_type : 	STRUCT 
+		Enable : BOOL;
+		Login : BOOL;
+		Logout : BOOL;
+	END_STRUCT;
+	UserXLoginStatus_type : 	STRUCT 
+		CommandDone : BOOL;
+		CurrentUser : WSTRING[50];
+		Error : BOOL;
+	END_STRUCT;
+	UserXLoginParameters_type : 	STRUCT 
+		UserName : STRING[50];
+		Password : UDINT;
+	END_STRUCT;
+END_TYPE
+
+(*HMIAudit control*)
+
+TYPE
+	HMIAuditInterface_type : 	STRUCT 
+		Commands : HMIAuditInterfaceCommands_type;
+		Status : HMIAuditInterfaceStatus_type;
+		Parameters : HMIAuditInterfaceParameters_type;
+	END_STRUCT;
+	HMIAuditInterfaceCommands_type : 	STRUCT 
+		ExportArchives : BOOL;
+	END_STRUCT;
+	HMIAuditInterfaceStatus_type : 	STRUCT 
+		AuditTrailCmdDone : BOOL;
+		AuditTrailArchiveAvailable : BOOL;
+	END_STRUCT;
+	HMIAuditInterfaceParameters_type : 	STRUCT 
+		Filler : BOOL;
+	END_STRUCT;
+END_TYPE
+
+(*MpRecipe control*)
+
+TYPE
+	MpRecipeUIConnect_type : 	STRUCT 
+		Parameters : MpRecipeUIConnectParameters_type;
+		Commands : MpRecipeUIConnectCommands_type;
+		Status : MpRecipeUIConnectStatus_type;
+	END_STRUCT;
+	MpRecipeUIConnectParameters_type : 	STRUCT 
+		FileName : USINT;
+	END_STRUCT;
+	MpRecipeUIConnectCommands_type : 	STRUCT 
+		New_Member : USINT;
+	END_STRUCT;
+	MpRecipeUIConnectStatus_type : 	STRUCT 
+		New_Member : USINT;
+	END_STRUCT;
+END_TYPE
